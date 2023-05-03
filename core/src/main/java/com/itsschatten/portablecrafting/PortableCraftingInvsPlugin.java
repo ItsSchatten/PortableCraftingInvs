2c0673a852514f65ff41d0b4390730e1bf369b1b
ItsSchatten <itsschatten@itsschatten.com>
2023-05-03 14:20:45 -0600

    Implement a hopeful fix to #41 Closes #40

--- core/src/main/java/com/itsschatten/portablecrafting/PortableCraftingInvsPlugin.java
+++ core/src/main/java/com/itsschatten/portablecrafting/PortableCraftingInvsPlugin.java
@@ -17,12 +17,14 @@ import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandMap;
+import org.bukkit.command.defaults.ReloadCommand;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -33,6 +35,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
 import java.util.Properties;
 import java.util.UUID;
 
@@ -200,52 +203,57 @@ public class PortableCraftingInvsPlugin extends JavaPlugin {
             if (!Settings.USE_CRAFTING) {
                 Utils.debugLog(Settings.DEBUG, "Crafting features have been disabled, and Essentials has been installed. To avoid causing issues we are not going to register the command.");
             } else {
-                registerCommand(new CraftCommand());
+                registerCommands(new CraftCommand());
             }
 
             if (!Settings.USE_ENDERCHEST) {
                 Utils.debugLog(Settings.DEBUG, "Enderchest features have been disabled, and Essentials has been installed. To avoid causing issues we are not going to register the command.");
             } else {
-                registerCommand(new EnderChestCommand());
+                registerCommands(new EnderChestCommand());
             }
 
             if (!Settings.USE_ANVIL) {
                 Utils.debugLog(Settings.DEBUG, "Anvil features have been disabled, and Essentials has been installed. To avoid causing issues we are not going to register the command.");
             } else {
-                registerCommand(new AnvilCommand());
+                registerCommands(new AnvilCommand());
             }
 
             if (!Settings.USE_GRINDSTONE) {
                 Utils.debugLog(Settings.DEBUG, "Grind stone features have been disabled, and Essentials has been installed. To avoid causing issues we are not going to register the command.");
             } else {
-                registerCommand(new GrindStoneCommand());
+                registerCommands(new GrindStoneCommand());
             }
 
             if (!Settings.USE_LOOM) {
                 Utils.debugLog(Settings.DEBUG, "Loom features have been disabled, and Essentials has been installed. To avoid causing issues we are not going to register the command.");
             } else {
-                registerCommand(new LoomCommand());
+                registerCommands(new LoomCommand());
             }
 
             if (!Settings.USE_STONE_CUTTER) {
                 Utils.debugLog(Settings.DEBUG, "Stonecutter features have been disabled, and Essentials has been installed. To avoid causing issues we are not going to register the command.");
             } else {
-                registerCommand(new StoneCutterCommand());
+                registerCommands(new StoneCutterCommand());
             }
 
             if (!Settings.USE_CARTOGRAPHY) {
                 Utils.debugLog(Settings.DEBUG, "Cartography table features have been disabled, and Essentials has been installed. To avoid causing issues we are not going to register the command.");
             } else {
-                registerCommand(new CartographyCommand());
+                registerCommands(new CartographyCommand());
             }
 
             if (!Settings.USE_SMITHING_SIGN) {
                 Utils.debugLog(Settings.DEBUG, "Smithing table features have been disabled, and Essentials has been installed. To avoid causing issues we are not going to register the command.");
             } else {
-                registerCommand(new SmithingCommand());
+                registerCommands(new SmithingCommand());
             }
 
         } else {
+
+            unregisterCommand(getServer().getPluginCommand("craft"), getServer().getPluginCommand("anvil"), getServer().getPluginCommand("enderchest"),
+                    getServer().getPluginCommand("grindstone"), getServer().getPluginCommand("stonecutter"), getServer().getPluginCommand("cartography"),
+                    getServer().getPluginCommand("smithing"));
+
             registerCommands(new AnvilCommand(), new GrindStoneCommand(), new EnderChestCommand(), new CraftCommand(),
                     new LoomCommand(), new StoneCutterCommand(), new CartographyCommand(), new SmithingCommand());
 
@@ -293,24 +301,13 @@ public class PortableCraftingInvsPlugin extends JavaPlugin {
         Utils.setInstance(null);
     }
 
-    // A utility method so I can register commands on one line.
-    private void registerCommand(Command command) {
-        try {
-            Utils.registerCommand(command);
-            Utils.debugLog(Settings.DEBUG, "&7Command " + command.getName() + " has been registered.");
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            Utils.log("Some error occurred, please report this immediately to ItsSchatten on Spigot or Github. \n(https://github.com/ItsSchatten/PortableCraftingInvs/issues)");
-        }
-    }
-
+    // Util method to register commands quickly.
     private void registerCommands(Command... commands) {
         try {
             for (Command command : commands) {
                 Utils.registerCommand(command);
                 Utils.debugLog(Settings.DEBUG, "&7Command " + command.getName() + " has been registered.");
             }
-
         } catch (Exception ex) {
             ex.printStackTrace();
             Utils.log("Some error occurred, please report this immediately to ItsSchatten on Spigot or Github. \n(https://github.com/ItsSchatten/PortableCraftingInvs/issues)");
@@ -318,12 +315,19 @@ public class PortableCraftingInvsPlugin extends JavaPlugin {
     }
 
     // Uses some reflection to access the server's command map and unregister out commands.
-    public void unregisterCommand(Command command) {
+    public void unregisterCommand(Command... commands) {
+        if (commands == null) return;
         try {
-            Field commandMapField = Bukkit.getServer().getClass().getDeclaredField("commandMap");
+            final Field commandMapField = Bukkit.getServer().getClass().getDeclaredField("commandMap");
             commandMapField.setAccessible(true);
-            CommandMap commandMap = (CommandMap) commandMapField.get(Bukkit.getServer());
-            command.unregister(commandMap);
+            final CommandMap commandMap = (CommandMap) commandMapField.get(Bukkit.getServer());
+
+            for (Command command : commands) {
+                if (command == null) continue;
+                command.unregister(commandMap);
+                Utils.registerCommand(command);
+                Utils.debugLog(Settings.DEBUG, "&7Command " + command.getName() + " has been unregistered.");
+            }
         } catch (IllegalAccessException | NoSuchFieldException e) {
             e.printStackTrace();
         }
